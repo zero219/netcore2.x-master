63c4a946f64e8a7afc16f57771c01a1ab529a594
zc Zhou <303368764@qq.com>
2020-03-17 14:28:56 +0800

    1

--- netcore2.Web/Controllers/AccountController.cs
+++ netcore2.Web/Controllers/AccountController.cs
@@ -11,7 +11,7 @@ namespace netcore2.Web.Controllers
     public class AccountController : Controller
     {
         private readonly SignInManager<ApplicationUser> _signInManager;
-           private readonly UserManager<ApplicationUser> _userManager;
+        private readonly UserManager<ApplicationUser> _userManager;
 
         public AccountController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
         {
